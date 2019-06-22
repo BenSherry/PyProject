0d44f2c62e7876ada7b7ec1f88ba5ca0d61b308e
yangchao <yangchao61@outlook.com>
2019-06-22 20:24:28 +0800

    add fake way of sort(bogo_sort) by c++

--- Alpha/sorts/c++/Knowledge/algorithm/sort.h
+++ Alpha/sorts/c++/Knowledge/algorithm/sort.h
@@ -4,10 +4,19 @@
 
 #ifndef KNOWLEDGE_SORT_H
 #define KNOWLEDGE_SORT_H
+#include<ctime>
+#include<algorithm>
 namespace algorithm
 {
     namespace sort
     {
+        template<typename T>
+        void showData(T data)
+        {
+            std::for_each(data.begin(),data.end(),[](int i){std::cout<<i<<" ";});
+            std::cout<< std::endl;
+        }
+
         template<typename T>
         void swap(T &left, T &right)
         {
@@ -35,6 +44,31 @@ namespace algorithm
                {
                    break;
                }
+           }
+       }
+
+       template<typename T>
+       bool is_sorted(T & data)
+       {
+           int len = data.size();
+           for (auto i = 0; i < len -1 ; i++)
+           {
+               if (data[i] > data[i+1])
+               {
+                   return false;
+               }
+           }
+           return true;
+       }
+
+       template<typename T>
+       void bogo_sort(T & wait_sort)
+       {
+           while (!is_sorted(wait_sort))
+           {
+               showData(wait_sort);
+               srand((unsigned int)time(0));
+               std::random_shuffle(wait_sort.begin(),wait_sort.end());
            }
        }
 
--- Alpha/sorts/c++/Knowledge/main.cpp
+++ Alpha/sorts/c++/Knowledge/main.cpp
@@ -4,16 +4,10 @@
 #include<algorithm>
 const unsigned int NMUS_NUMBER = 10;
 using array_sort = std::array<int, NMUS_NUMBER>;
-template<typename T>
-void showData(T data)
-{
-    std::for_each(data.begin(),data.end(),[](int i){std::cout<<i<<" ";});
-    std::cout<< std::endl;
-}
+
 int main() {
     array_sort wait_sort {2, 1 ,5 ,6, 3, 2, 67, 34};
+    std::cout<<algorithm::sort::is_sorted(wait_sort)<<std::endl;
     algorithm::sort::bubble_sort<array_sort>(wait_sort);
-    std::cout << "Hello, World!" << std::endl;
-    showData(wait_sort);
     return 0;
 }
